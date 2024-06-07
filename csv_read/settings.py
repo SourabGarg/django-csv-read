94ac95cfbb69329d34cf05bccc026f0cf23c91ab
SourabGarg <sharmasourab502@gmail.com>
2024-06-07 23:43:35 +0530

    Add app and HTML

--- csv_read/settings.py
+++ csv_read/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'result',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'csv_read.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "csv_read" / "templates",
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ result/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+
+def result(request):
+    return render(request, "result/result.html")
+
